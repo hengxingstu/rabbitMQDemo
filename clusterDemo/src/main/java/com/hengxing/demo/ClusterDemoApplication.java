5c6ed02a9f8a4a0faa8112f42eac4dd20c1935d3
hengxing <hengxingstu@163.com>
2023-07-23 21:08:13 +0800

    初始化集群

---
+++ clusterDemo/src/main/java/com/hengxing/demo/ClusterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hengxing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusterDemoApplication.class, args);
+	}
+
+}
---
+++ clusterDemo/src/test/java/com/hengxing/demo/ClusterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hengxing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
